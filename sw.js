528d38092d664732864f1a4675e45afe5c5e628d
betty54251234-pixel <betty54251234@gmail.com>
2026-08-11 17:11:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,11 @@
-const CACHE='trippath-v2.2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
-));
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(url.origin!==location.origin || e.request.method!=='GET') return;
-  e.respondWith(
-    fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;})
-      .catch(()=>caches.match(e.request))
-  );
+const OLD_CACHE_PREFIX='trippath-';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{
+  e.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.filter(k=>k.startsWith(OLD_CACHE_PREFIX)).map(k=>caches.delete(k)));
+    await self.registration.unregister();
+    const clients=await self.clients.matchAll({type:'window'});
+    for(const c of clients){try{c.navigate(c.url)}catch(_){}}
+  })());
 });
